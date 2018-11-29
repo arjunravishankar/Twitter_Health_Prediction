38ee8cb0e10f530e77041d897ee342568c21d7fe
jbernabei <john.bernabei@uphs.upenn.edu>
2018-11-29 13:12:35 -0500

    Add files via upload

--- predict_labels.m
+++ predict_labels.m
@@ -1,27 +1,131 @@
-function pred_labels=predict_labels(train_inputs,train_labels,test_inputs)
-% Redefine variables
+function [pred_labels]=predict_labels(train_inputs,train_labels,test_inputs)
+%% Load and import data
+% Get given X train/test, Y_train
 X_train = train_inputs;
-Y_train = train_labels;
 X_test  = test_inputs;
 
-[n, p] = size(X_train);
-labels = size(Y_train,2);
+Y_train = train_labels;
 
-X_train_reduce = (X_train(:,2:end));
-X_test_reduce  = (X_test(:,2:end));
+% Get other training data
+load train_data
+load features
 
-num_trees = 40;
+X_train_tot = [X_train];
+Y_train_tot = [Y_train];
 
-for j=1:labels
-                mdl(j).data = TreeBagger(num_trees,X_train_reduce,Y_train(:,j),...
-                            'oobpred','On','Method','regression',...
-                            'OOBVarImp','on');
-                Y_pred_rf(:,j) = predict(mdl(j).data,X_test_reduce);
+X_tot = [X_train;X_test];
+
+% Get sizes of data
+[n_train, p] = size(X_train_tot);
+[n_test, p] = size(X_test);
+labels = size(train_labels,2);
+
+%% get final features
+final_feats = [];
+for j = 1:9
+   final_feats = [final_feats, mMSEpred(j).data];
 end
 
-pred_labels = Y_pred_rf
+fin_feats = unique(final_feats);
+final_feats = fin_feats(1<histc(final_feats,fin_feats));
 
-%pred_labels=randn(size(test_inputs,1),size(train_labels,2));
+%% Process data
+% Extract tweets
+%X_tweet = train_inputs(:,tweet_ind);
+X_tweet = X_tot(:,22:end);
+
+
+% Extract non-redundant demographics
+% Demographic columns 7-10, 21 data are contained in column 11
+X_demo = X_tot(:,[1:6,11:20]);
+
+%% Reduce data
+% PCA tweet data
+[COEFF, SCORE, LATENT, TSQUARED, EXPLAINED, MU] = pca(X_tweet(:,(final_feats(final_feats>21)-21)));
+X_tweet_pca = SCORE(:,1:30);
+
+%% Get final data
+X_demo_train = X_demo((1:n_train),:);
+X_tweet_pca_train = X_tweet_pca((1:n_train),:);
+X_comp_train = [X_demo_train,X_tweet_pca_train];
+Z_comp_train = zscore(X_comp_train);
+
+X_demo_test = X_demo((n_train+1):end,:);
+X_tweet_pca_test = X_tweet_pca((n_train+1):end,:);
+X_comp_test = [X_demo_test, X_tweet_pca_test];
+Z_comp_test = zscore(X_comp_test);
+
+
+Y_train = Y_train_tot;
+
+%% Do lasso
+for j=1:9
+    [B,FitInfo] = lasso(X_train_tot(:,final_feats),Y_train(:,j),'Alpha',0.5,'MaxIter',1e4,'CV',5)%,'PredictorNames',string([1:size(X_comp_train,2)]));
+    idxLambda1SE = FitInfo.Index1SE;
+    coef = B(:,idxLambda1SE);
+    coef0 = FitInfo.Intercept(idxLambda1SE);
+    Y_est_el(:,j) = X_train_tot(:,final_feats)*coef + coef0;
+    Y_pred_el(:,j) = X_test(:,final_feats)*coef + coef0;
+    %idxLambdaMinMSE = FitInfo.IndexMinMSE;
+    %mMSEpred(j).data = cellfun(@str2num,FitInfo.PredictorNames(B(:,idxLambdaMinMSE)~=0))
+   if j ~= 3 && j ~= 8
+        Y_est_el(:,j) = round(Y_est_el(:,j));
+        Y_pred_el(:,j) = round(Y_pred_el(:,j));
+   else
+        Y_est_el(:,j) = round(Y_est_el(:,j),1);
+        Y_pred_el(:,j) = round(Y_pred_el(:,j),1); 
+   end
+end
+
+%% neural net
+
+for j = 1:9
+    net = feedforwardnet(30);
+    net.layers{1}.transferFcn = 'poslin';% 1st layer activation function is logistic sigmoid
+    net.performFcn = 'crossentropy' ;  %specify loss function appropriate for classification
+    net.performParam.regularization = 10e-3 ;  % regularization parameter
+    net = trainlm(net,Z_comp_train',Y_train(:,j)'); % train with backpropagation
+    Y_est_ne(:,j) = net(Z_comp_train');
+    Y_pred_ne(:,j)= net(Z_comp_test');
+end
+
+
+%% Discriminative model - random forest
+
+for j = 1:labels
+    mdl_rf(j).data = TreeBagger(70,X_comp_train,Y_train(:,j),...
+                'oobpred','On','Method','regression',...
+                'OOBVarImp','on');   
+    Y_est_rf(:,j) = predict(mdl_rf(j).data,X_comp_train);
+    Y_pred_rf(:,j) = predict(mdl_rf(j).data,X_comp_test);
+   if j ~= 3 && j ~= 8
+       Y_est_rf(:,j) = round(Y_est_rf(:,j));
+        Y_pred_rf(:,j) = round(Y_pred_rf(:,j));
+   else
+       Y_est_rf(:,j) = round(Y_est_rf(:,j),1);
+        Y_pred_rf(:,j) = round(Y_pred_rf(:,j),1); 
+   end
+end
+
+
+%% Instance based model - knn
+
+for j = 1:labels
+    Y_est_kr(:,j) = k_nearest_neighbours(Z_comp_train,Y_train(:,j),Z_comp_train,20,'l2');
+    Y_pred_kr(:,j) = k_nearest_neighbours(Z_comp_train,Y_train(:,j),Z_comp_test,20,'l2');
+end
+
+%% Ensemble
+
+for j=1:labels
+   mdl_ens(j).data = TreeBagger(30,[Y_est_rf, Y_est_kr, Y_est_ne, Y_est_el],Y_train(:,j),...
+                'oobpred','On','Method','regression',...
+                'OOBVarImp','on'); 
+   Y_pred_ens(:,j) = predict(mdl_ens(j).data,[Y_pred_rf, Y_pred_kr, Y_pred_ne, Y_pred_el]);
+end
+
+%% Return labels
+pred_labels = Y_pred_ens;
 
 end
 
