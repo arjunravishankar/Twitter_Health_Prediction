5f94397823a6ff59c289421616eeaeefd37b4552
arjunravishankar <sarjunshankar@gmail.com>
2018-12-01 10:30:17 -0500

    Add files via upload

--- LDAMapping.m
+++ LDAMapping.m
@@ -2,16 +2,17 @@
 % Calculate Mapped Training Data and Mapping to convert other feature
 % matrices to best LDA components in feature space
 for j=1:9
-[X_lda_train{j}, Mapping{j}]= lda(train_inputs,train_labels(:,j), 400);
+    j
+    [X_lda_train{j}, Mapping{j}]= lda(train_inputs,train_labels(:,j), 400);
 end
 
-% Convert any test feature matrix to best LDA components 
+% Convert any test feature matrix to best LDA components
 
 % Make sure data is zero mean
 feature_mean=mean(test_inputs, 1);
 X = bsxfun(@minus, test_inputs, feature_mean);
 
 % Compute mapped data
-for j=1:3
-X_lda_test{j} = X * Mapping{j}.M;
+for j=1:9
+    X_lda_test{j} = X * Mapping{j}.M;
 end
