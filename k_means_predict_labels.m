2e87127897f6947b217634c1d5156b19509f4f7b
arjunravishankar <sarjunshankar@gmail.com>
2018-12-07 14:30:42 -0500

    Add files via upload

--- k_means_predict_labels.m
+++ k_means_predict_labels.m
@@ -5,18 +5,20 @@ function [pred_labels]=k_means_predict_labels(train_inputs,train_labels,test_inp
 pred_labels=zeros(size(test_inputs,1),p_features);
 
 %Loop through each label column to create predicted labels matrix
+[idx,C] = kmeans(vertcat(train_inputs,test_inputs),100);
 for p=1:p_features
-    
-    [idx,C] = kmeans(train_inputs,length(unique(train_labels(:,p))));
-    cluster_labels=zeros(length(unique(train_labels(:,p))),1);
-    
-    for cluster=1:length(unique(train_labels(:,p)))
-        cluster_labels(cluster)=mean(train_labels(idx==cluster,p));
+    cluster_labels=zeros(k,1);
+    for cluster=1:k
+        cluster_labels(cluster)=mean(train_labels(idx(1:size(train_inputs,1))==cluster,p));
     end
+    pred_labels(:,p) =cluster_labels(idx((size(train_inputs,1)+1):end));
     
-    distance=pdist2(test_inputs,C,'Euclidean');
+    %Replace NaNs with nearest non Nan cluster_label
+    distance=pdist2(test_inputs,C(~isnan(cluster_labels),:),'Euclidean');
     [~,I] = min(distance,[],2);
-    pred_labels(:,p) = cluster_labels(I);
+    NonNan_cluster_labels=cluster_labels(~isnan(cluster_labels));
+    backup_test_cluster_labels=NonNan_cluster_labels(I);
+    pred_labels(isnan(pred_labels(:,p)),p)=backup_test_cluster_labels(isnan(pred_labels(:,p)));
 end
 
 end
